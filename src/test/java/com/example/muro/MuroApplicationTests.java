016f393bcfe48843bf6d9b9be0450b7a87b24de0
Sowon <cherishwish12@gmail.com>
2023-11-13 00:01:54 +0900

    Initial commit

---
+++ src/test/java/com/example/muro/MuroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
